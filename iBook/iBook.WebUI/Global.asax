37b8c2339897c12736399f7f915ba1aefd96383f
MEHEDI <me.mehedi@hotmail.com>
2020-02-10 14:38:02 +0600

    Initial commit

---
+++ iBook/iBook.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="iBook.WebUI.MvcApplication" Language="C#" %>
---
+++ iBook/iBook.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iBook.WebUI.Startup))]
+namespace iBook.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
